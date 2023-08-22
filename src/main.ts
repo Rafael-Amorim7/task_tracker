39aaf7910567ac87ae385d38d698ebf75fa36a84
Rafael de Carvalho Amorim <rafael.amorim@contabilivre.com.br>
2023-08-22 10:56:56 -0300

    cookie lib

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,10 @@ import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import { VueCookieNext } from 'vue-cookie-next'
 
-createApp(App).mount('#app')
+VueCookieNext.config({ expire: '7d' });
+
+const app = createApp(App);
+app.use(VueCookieNext);
+app.mount('#app');
